44abebf9a1be92e24bfda7e0e7ee9c465eb8bc39
Allen-zc <1053831730@qq.com>
2023-07-25 08:48:29 +0800

    p45 0:0 三級分類

--- gulimall-member/src/main/java/com/atguigu/gulimall/member/GulimallMemberApplication.java
+++ gulimall-member/src/main/java/com/atguigu/gulimall/member/GulimallMemberApplication.java
@@ -1,8 +1,16 @@
 package com.atguigu.gulimall.member;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+/*开启openfeign远程调用功能*/
+@EnableFeignClients(basePackages = "com.atguigu.gulimall.member.feign")
+/*开启nacos服务的注册发现功能*/
+@EnableDiscoveryClient
+@MapperScan("com.atguigu.gulimall.member.dao")
 @SpringBootApplication
 public class GulimallMemberApplication {
 
